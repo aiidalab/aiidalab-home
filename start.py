f567417df0a7603051cad97d598cc6e46925ff91
Ole Schuett <ole@schuett.name>
2017-11-18 14:01:06 +0100

    reformat start widget

--- start.py
+++ start.py
@@ -3,21 +3,44 @@ import ipywidgets as ipw
 def get_start_widget(appbase, jupbase):
     #http://fontawesome.io/icons/
     template = """
-<a style="{style}" href="{jupbase}/tree/" title="File Browser" target="_blank">
-<i class="fa fa-file-text-o fa-4x"></i>
-</a>
+    <table>
+    <tr>
 
-<a style="{style}" href="{appbase}/terminal.ipynb" title="Open Terminal" target="_blank">
-<i class="fa fa-terminal fa-4x"></i>
-</a>
+    <td style="text-align:center">
+    <a style="{style}" href="{jupbase}/tree/" title="File Browser" target="_blank">
+    <i class="fa fa-file-text-o fa-4x"></i>
+    </a><br>
+    File Browser
+    </td>
 
-<a style="{style}" href="{jupbase}/tree/#running" title="Task Manager" target="_blank">
-<i class="fa fa-tasks fa-4x"></i>
-</a>
+    <td style="width:70px"></td>
 
-<a style="{style}" href="{appbase}/appmanager.ipynb" title="Manage Apps">
-<i class="fa fa-cog fa-4x" aria-hidden="true"></i>
-</a>
+    <td style="text-align:center">
+    <a style="{style}" href="{appbase}/terminal.ipynb" title="Open Terminal" target="_blank">
+    <i class="fa fa-terminal fa-4x"></i>
+    </a><br>
+    Terminal
+    </td>
+
+    <td style="width:70px"></td>
+
+    <td style="text-align:center">
+    <a style="{style}" href="{jupbase}/tree/#running" title="Task Manager" target="_blank">
+    <i class="fa fa-tasks fa-4x"></i>
+    </a><br>
+    Tasks
+    </td>
+
+    <td style="width:70px"></td>
+
+    <td style="text-align:center">
+    <a style="{style}" href="{appbase}/appmanager.ipynb" title="Manage Apps">
+    <i class="fa fa-cog fa-4x" aria-hidden="true"></i>
+    </a><br>
+    Manage Apps
+    </td>
+    </tr>
+    </table>
 """
     
     html = template.format(appbase=appbase, jupbase=jupbase, style="margin:20px")
