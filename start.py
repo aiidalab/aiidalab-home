4e0a6e7a16176bd3a0dd9d9d5fb73e39b56caa34
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-12-21 15:10:48 +0000

    Redesigning app store, first working version:
    1) Home page:
       - "update available" information per every app
       - "Manage App" button per every app
       - git link per every app
       - inform if the app is present, but does not have a remote repository
       - inform if app is not managed by git
    
    2) App "home page"
       - app logo, authors and description. If no logo available - the default
         one will be taken
       - uninstall, update, install buttons
       - use local metadata for the installed apps
    
    3) Use dulwich to interact with Git.
       - if commited local changes: do not allow to delete the app
       - if not commited: do not allow to delete the app and/or
         switch the branchd
       - if found remote changes: activate the update button
       - get git-related information from the aiidalab server that
         merges apps.json, metadata and information about git into
         single file named apps_meta.json
       - handle the situations when the apps_meta.json file can't be
         downloaded
    
    4) App Store
       - limited app description is shown
       - ability to select number of apps per page
       - add a filter to show only installed apps
       - add groups for the apps
    
    5) Home app
       - make the home app frozen on top
       - rename icons, replace App Store icon, add meaningful description
       - add "Help" symbol to the home app
       - remove border from the home app so that it looks different from the
         others

--- start.py
+++ start.py
@@ -3,21 +3,22 @@ import ipywidgets as ipw
 def get_start_widget(appbase, jupbase):
     #http://fontawesome.io/icons/
     template = """
+    <center>
     <table>
     <tr>
 
     <td style="text-align:center">
-    <a style="{style}" href="{jupbase}/tree/" title="File Browser" target="_blank">
-    <i class="fa fa-file-text-o fa-4x"></i>
+    <a style="{style}" href="{jupbase}/tree/" title="File Manager" target="_blank">
+    <i class="fa fa-file-text-o fa-3x"></i>
     </a><br>
-    File Browser
+    File Manager
     </td>
 
     <td style="width:70px"></td>
 
     <td style="text-align:center">
     <a style="{style}" href="{appbase}/terminal.ipynb" title="Open Terminal" target="_blank">
-    <i class="fa fa-terminal fa-4x"></i>
+    <i class="fa fa-terminal fa-3x"></i>
     </a><br>
     Terminal
     </td>
@@ -26,7 +27,7 @@ def get_start_widget(appbase, jupbase):
 
     <td style="text-align:center">
     <a style="{style}" href="{jupbase}/tree/#running" title="Task Manager" target="_blank">
-    <i class="fa fa-tasks fa-4x"></i>
+    <i class="fa fa-tasks fa-3x"></i>
     </a><br>
     Tasks
     </td>
@@ -34,13 +35,23 @@ def get_start_widget(appbase, jupbase):
     <td style="width:70px"></td>
 
     <td style="text-align:center">
-    <a style="{style}" href="{appbase}/appstore.ipynb" title="App Store">
-    <i class="fa fa-cog fa-4x" aria-hidden="true"></i>
+    <a style="{style}" href="{appbase}/appstore.ipynb" title="Install New Apps">
+    <i class="fa fa-puzzle-piece fa-3x" aria-hidden="true"></i>
     </a><br>
-    Manage Apps
+    App Store
+    </td>
+
+    <td style="width:70px"></td>
+
+    <td style="text-align:center">
+    <a style="{style}" href="https://github.com/aiidalab/aiidalab-home/wiki/Intro" title="Learn about AiiDA lab" target="_blank">
+    <i class="fa fa-question fa-3x" aria-hidden="true"></i>
+    </a><br>
+    Help
     </td>
     </tr>
     </table>
+    </center>
 """
     
     html = template.format(appbase=appbase, jupbase=jupbase, style="margin:20px")
