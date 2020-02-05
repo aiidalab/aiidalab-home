84442d49c0e6c445766a895da75287c3170285f4
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-02-05 21:49:59 +0100

    Move core functionality to aiidalab package. (#30)
    This PR extracts core functionality that is now transferred to
    aiidalab package.
    
    related PR: https://github.com/aiidalab/aiidalab/pull/27

--- config.py
+++
@@ -1,5 +0,0 @@
-from os import getenv
-
-aiidalab_home = getenv('AIIDALAB_HOME', '/project')
-aiidalab_apps = getenv('AIIDALAB_APPS', '/project/apps')
-aiidalab_scripts = getenv('AIIDALAB_SCRIPTS', '/opt')
