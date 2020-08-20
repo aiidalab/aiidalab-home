f589c0da22047f4a660df0dd0f20f5697f186504
Dou Du <dou.du.epfl@gmail.com>
2020-08-20 17:48:09 +0200

    Add test for the terminal

---
+++ tests/test_terminal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
+from time import sleep 
+
+
+def test_terminal(selenium, url):
+    selenium.get(url("apps/apps/home/start.ipynb"))
+    selenium.set_window_size(1575, 907)
+    selenium.find_element(By.CSS_SELECTOR, ".fa-terminal").click()
+    page = selenium.window_handles[-1]
+    sleep(3)
+    selenium.switch_to.window(page)
+    selenium.find_element(By.CSS_SELECTOR, ".xterm-cursor-layer").click()
+    selenium.find_element(By.CSS_SELECTOR, ".xterm-helper-textarea").send_keys("jupyter --version") 
+    selenium.find_element(By.CSS_SELECTOR, ".xterm-helper-textarea").send_keys(Keys.ENTER) 
+    sleep(1)
+    selenium.get_screenshot_as_file(f'screenshots/aiidalab-terminal.png')
