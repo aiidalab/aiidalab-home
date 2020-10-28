62a0d49708522434ffc07c26c64500914af1b08e
Simon Adorf <simon.adorf@epfl.ch>
2020-10-28 09:49:35 +0100

    Update app management uninstall-install unit test.

--- tests/test_manage_app.py
+++ tests/test_manage_app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 def test_uninstall_install_widgets_base(selenium, url):
     selenium.get(url("apps/apps/home/single_app.ipynb?app=aiidalab-widgets-base"))
-    selenium.set_window_size(1575, 907)
+    selenium.set_window_size(1440, 828)
     selenium.find_element(By.XPATH, "//button[contains(.,\'Uninstall\')]").click()
     selenium.get_screenshot_as_file(f'screenshots/manage-app-aiidalab-widgets-base-uninstalled.png')
     selenium.find_element(By.XPATH, "//button[contains(.,\'Install\')]").click()
     selenium.get_screenshot_as_file(f'screenshots/manage-app-aiidalab-widgets-base-installed.png')
-    selenium.find_element(By.XPATH, "//div[@id=\'notebook-container\']/div[5]/div[2]/div[2]/div/div[3]/div/div[2]/div[2]/select").click()
-    selenium.find_element(By.CSS_SELECTOR, ".widget-dropdown > .widget-label").click()
-    selenium.get_screenshot_as_file(f'screenshots/manage-app-aiidalab-widgets-base.png')
