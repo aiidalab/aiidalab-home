02486c4dbde32cc3b99a9279b4b2785d7a4a1412
Simon Adorf <simon.adorf@epfl.ch>
2021-07-14 09:38:01 +0200

    Add pre-commit checks and clean up package code.

--- home/utils.py
+++ home/utils.py
@@ -1,16 +1,16 @@
 """Helpful utilities for the AiiDAlab tools."""
 
 import sys
-from os import path
 from importlib import import_module
+from os import path
 
-from markdown import markdown
 import ipywidgets as ipw
 from aiidalab.config import AIIDALAB_APPS
+from markdown import markdown
 
 
 def load_widget(name):
-    if path.exists(path.join(AIIDALAB_APPS, name, 'start.py')):
+    if path.exists(path.join(AIIDALAB_APPS, name, "start.py")):
         return load_start_py(name)
     return load_start_md(name)
 
@@ -18,12 +18,14 @@ def load_widget(name):
 def load_start_py(name):
     """Load app appearance from a Python file."""
     try:
-        mod = import_module('apps.%s.start' % name)
+        mod = import_module("apps.%s.start" % name)
         appbase = "../" + name
         jupbase = "../../.."
         notebase = jupbase + "/notebooks/apps/" + name
         try:
-            return mod.get_start_widget(appbase=appbase, jupbase=jupbase, notebase=notebase)
+            return mod.get_start_widget(
+                appbase=appbase, jupbase=jupbase, notebase=notebase
+            )
         except TypeError:
             return mod.get_start_widget(appbase=appbase, jupbase=jupbase)
     except Exception:  # pylint: disable=broad-except
@@ -32,7 +34,7 @@ def load_start_py(name):
 
 def load_start_md(name):
     """Load app appearance from a Markdown file."""
-    fname = path.join(AIIDALAB_APPS, name, 'start.md')
+    fname = path.join(AIIDALAB_APPS, name, "start.md")
     try:
 
         md_src = open(fname).read()
@@ -40,7 +42,7 @@ def load_start_md(name):
         html = markdown(md_src)
 
         # open links in new window/tab
-        html = html.replace('<a ', '<a target="_blank" ')
+        html = html.replace("<a ", '<a target="_blank" ')
 
         # downsize headings
         html = html.replace("<h3", "<h4")
@@ -54,15 +56,20 @@ def load_logo(app):
     """Return logo object. Give the priority to the local version"""
 
     # For some reason standard ipw.Image() app does not work properly.
-    res = ipw.HTML('<img src="./aiidalab_logo_v4.svg">', layout={'width': '100px', 'height': '100px'})
+    res = ipw.HTML(
+        '<img src="./aiidalab_logo_v4.svg">',
+        layout={"width": "100px", "height": "100px"},
+    )
 
     # Checking whether the 'logo' key is present in metadata dictionary.
-    if 'logo' not in app.metadata:
+    if "logo" not in app.metadata:
         res.value = '<img src="./aiidalab_logo_v4.svg">'
 
     # If 'logo' key is present and the app is installed.
     elif app.is_installed():
-        res.value = '<img src="{}">'.format(path.join('..', app.name, app.metadata['logo']))
+        res.value = '<img src="{}">'.format(
+            path.join("..", app.name, app.metadata["logo"])
+        )
 
     # If not installed, getting file from the remote git repository.
     else:
@@ -70,11 +77,11 @@ def load_logo(app):
         html_link = path.splitext(app.url)[0]
 
         # We expect it to always be a git repository
-        html_link += '/master/' + app.metadata['logo']
-        if 'github.com' in html_link:
-            html_link = html_link.replace('github.com', 'raw.githubusercontent.com')
-            if html_link.endswith('.svg'):
-                html_link += '?sanitize=true'
+        html_link += "/master/" + app.metadata["logo"]
+        if "github.com" in html_link:
+            html_link = html_link.replace("github.com", "raw.githubusercontent.com")
+            if html_link.endswith(".svg"):
+                html_link += "?sanitize=true"
         res.value = '<img src="{}">'.format(html_link)
 
     return res
--- tests/test_start.py
+++ tests/test_start.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from selenium.webdriver.common.by import By
-from contextlib import contextmanager
 import time
+from contextlib import contextmanager
+
+from selenium.webdriver.common.by import By
+
 
 @contextmanager
 def get_new_windows(selenium, timeout=2):
@@ -19,12 +21,16 @@ def test_click_appstore(selenium, url):
     assert len(handles) == 1
     selenium.switch_to.window(handles.pop())
     time.sleep(5)
-    dropdown = selenium.find_element(By.XPATH, "//div[@id=\'notebook-container\']/div[5]/div[2]/div[2]/div/div[3]/div/div[2]/div/select")
+    dropdown = selenium.find_element(
+        By.XPATH,
+        "//div[@id='notebook-container']/div[5]/div[2]/div[2]/div/div[3]/div/div[2]/div/select",
+    )
     dropdown.find_element(By.XPATH, "//option[. = 'Utilities']").click()
     selenium.find_element(By.CSS_SELECTOR, ".widget-button:nth-child(1)").click()
     selenium.find_element(By.CSS_SELECTOR, ".widget-html-content > h1").click()
     time.sleep(5)
-    selenium.get_screenshot_as_file(f'screenshots/app-store.png')
+    selenium.get_screenshot_as_file("screenshots/app-store.png")
+
 
 def test_click_help(selenium, url):
     selenium.get(url("apps/apps/home/start.ipynb"))
@@ -34,7 +40,8 @@ def test_click_help(selenium, url):
     assert len(handles) == 1
     selenium.switch_to.window(handles.pop())
     selenium.find_element(By.CSS_SELECTOR, ".mr-md-2").click()
-    selenium.get_screenshot_as_file(f'screenshots/help.png')
+    selenium.get_screenshot_as_file("screenshots/help.png")
+
 
 def test_click_filemanager(selenium, url):
     selenium.get(url("apps/apps/home/start.ipynb"))
@@ -46,4 +53,4 @@ def test_click_filemanager(selenium, url):
     selenium.find_element(By.LINK_TEXT, "Running").click()
     selenium.find_element(By.LINK_TEXT, "Clusters").click()
     selenium.find_element(By.LINK_TEXT, "Files").click()
-    selenium.get_screenshot_as_file(f'screenshots/file-manager.png')
+    selenium.get_screenshot_as_file("screenshots/file-manager.png")
