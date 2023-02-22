f423205b72e77f8dc4209de90e4d7a1fcd619b23
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-02-22 17:01:57 +0000

    Enlarge output from a running installation (#140)
    Enlarging the console output from a running installation makes it easier to see what is happening.

--- home/app_manager.py
+++ home/app_manager.py
@@ -118,7 +118,7 @@ class AppManagerWidget(ipw.VBox):
         )  # show empty line by default
 
         self.dependencies_log = LogOutputWidget(
-            layout=ipw.Layout(min_height="0px", max_height="100px")
+            layout=ipw.Layout(min_height="0px", max_height="400px")
         )  # max_height controls the maximum height of the log field.
         self.dependencies_log.template = (
             "Installing dependencies..." + self.dependencies_log.template
