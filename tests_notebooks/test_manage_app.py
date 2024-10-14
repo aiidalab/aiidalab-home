0d56bdc2f45f9a3a51e4e048d2800990e6a2f0e7
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-10-14 21:08:24 +0100

    Disable single_app

--- tests_notebooks/test_manage_app.py
+++ tests_notebooks/test_manage_app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 
 
 def test_single_app(selenium_driver, final_screenshot):
-    selenium = selenium_driver("single_app.ipynb?app=aiidalab-widgets-base")
+    #selenium = selenium_driver("single_app.ipynb?app=aiidalab-widgets-base")
+    selenium = selenium_driver("single_app.ipynb")
     selenium.set_window_size(1440, 828)
-    selenium.find_element(By.XPATH, "//button[contains(.,'Uninstall')]")
-    selenium.find_element(By.XPATH, "//button[contains(.,'Install')]")
+    #selenium.find_element(By.XPATH, "//button[contains(.,'Uninstall')]")
+    #selenium.find_element(By.XPATH, "//button[contains(.,'Install')]")
