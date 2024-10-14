d7cbf4c81b7e34b6e7fc971ce465ea8c3dcb71f4
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-10-14 23:11:31 +0100

    Window size

--- tests_notebooks/test_manage_app.py
+++ tests_notebooks/test_manage_app.py
@@ -5,5 +5,6 @@ def test_single_app(selenium_driver, final_screenshot):
     url_params = {"app": "aiidalab-widgets-base"}
     selenium = selenium_driver("single_app.ipynb", url_params)
     selenium.set_window_size(1440, 828)
+    selenium.set_window_size(950, 1000)
     selenium.find_element(By.XPATH, "//button[contains(.,'Uninstall')]")
     selenium.find_element(By.XPATH, "//button[contains(.,'Install')]")
