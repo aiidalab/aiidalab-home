34d8ffad3b60d4da1c8739a09576b043431f188d
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-10-14 22:37:46 +0100

    aiidalab-widgets-base single app

--- tests_notebooks/test_manage_app.py
+++ tests_notebooks/test_manage_app.py
@@ -4,8 +4,7 @@ from time import sleep
 
 
 def test_single_app(selenium_driver, final_screenshot):
-    # selenium = selenium_driver("single_app.ipynb?app=aiidalab-widgets-base")
-    selenium = selenium_driver("single_app.ipynb")
+    selenium = selenium_driver("single_app.ipynb?app=aiidalab-widgets-base")
     selenium.set_window_size(1440, 828)
     # selenium.find_element(By.XPATH, "//button[contains(.,'Uninstall')]")
     # selenium.find_element(By.XPATH, "//button[contains(.,'Install')]")
