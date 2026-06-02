5aa52b78fec8cc6653b7c01eb674ebbd88a857be
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2026-06-02 08:20:05 +0000

    Node preview and AWB_UNAVAILABLE_MESSAGE

--- home/node_preview.py
+++ home/node_preview.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import ipywidgets as ipw
 from aiida import orm
 
 AWB_UNAVAILABLE_MESSAGE = (
@@ -15,6 +16,7 @@ def render_node_preview(node: orm.Node) -> object:
     try:
         from aiidalab_widgets_base import viewer  # noqa: PLC0415
     except ImportError:
-        return AWB_UNAVAILABLE_MESSAGE
-
+        return ipw.HTML(
+            value=str(node) + "<br><em>" + AWB_UNAVAILABLE_MESSAGE + "</em>"
+        )
     return viewer(node)
